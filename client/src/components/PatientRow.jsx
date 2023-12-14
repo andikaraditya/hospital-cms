278aa8e19a19452c7118e48bdcf2ecf8b56ade42
Andika <andikaraditya1@gmail.com>
2023-12-14 18:42:48 +0700

    create side profile for patient detail  page

--- client/src/components/PatientRow.jsx
+++ client/src/components/PatientRow.jsx
@@ -1,14 +1,21 @@
 import "./PatientRow.scss"
+import { useNavigate } from "react-router-dom";
 
 function PatientRow(props) {
     const {patient} = props
+    const navigate = useNavigate()
     return (
         <div className="patient-row">
             <img src="https://cdn-icons-png.flaticon.com/512/1430/1430453.png" alt="" />
             <p>{patient.name}</p>
             <p>{patient.illness}</p>
             <p>{patient.Doctor.name}</p>
-            <button>Details</button>
+            <button
+            className="pointer-hover"
+            onClick={(e) => {
+                navigate(`/patients/${patient.id}`)
+            }}
+            >Details</button>
         </div>
     )
 }
--- client/src/views/PatientDetailPage.jsx
+++ client/src/views/PatientDetailPage.jsx
@@ -1,8 +1,55 @@
+import { useEffect, useState } from "react";
+import "./PatientDetailPage.scss"
+import axios from "axios";
+import { useParams } from "react-router-dom";
 function PatientDetailPage() {
+    const [patient, setPatient] = useState({})
+
+    const {id} = useParams()
+    async function fetchPatient() {
+        try {
+            const {data} = await axios({
+                method: "get",
+                url: `http://localhost:3000/Patients/${id}`,
+                params: {
+                    "_expand": ["Doctor", "Ward", "PatientInfo"]
+                }
+            })
+            console.log(data)
+            setPatient(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchPatient()
+    }, [])
+
+    if (patient.name === undefined) {
+        return (
+            <>
+            </>
+        )
+    }
+
     return (
-        <>
-            <h1>Patient Detail Page</h1>
-        </>
+        <div id="detail-page">
+            <div id="side-profile">
+                <img src="https://cdn-icons-png.flaticon.com/512/1430/1430453.png" alt="" />
+                <h1>{patient.name}</h1>
+                <div className="profile-info">
+                    <img src="https://cdn-icons-png.flaticon.com/512/3774/3774299.png" alt="" />
+                    <p>{patient.Doctor.name}</p>
+                </div>
+                <div className="profile-info">
+                    <img src="https://cdn-icons-png.flaticon.com/512/2185/2185005.png" alt="" />
+                    <p>{patient.Ward.name}</p>
+                </div>
+            </div>
+            <div id="info-detail">
+            </div>
+        </div>
     );
 }
 
